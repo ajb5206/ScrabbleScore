00264549777a223e5d260f7760f863e511cd98f4
Alex Bertotto <ajb5206@gmail.com>
2021-09-08 20:05:45 -0700

    add working code
    Co-Authored By Tim Roth <timdroth@gmail.com>
    Co-Authored By DeAunt'e Hall <godsofolympus88@gmail.com>
    Co-Authored By Mary Marks <maryleemarks@gmail.com>

--- ScrabbleScore/Models/Program.cs
+++ ScrabbleScore/Models/Program.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System;
 using ScrabbleScore.Models;
+using System.Linq;
 
 namespace ScrabbleScore.Models 
 {
@@ -8,32 +9,45 @@ namespace ScrabbleScore.Models
 	{
 		public static void Main()
 		{
-			Dictionary<char, int> letterScores = new Dictionary<char, int>() { {'a',1 }, {'e', 1} , {'i',1 } };
+			Dictionary<char, int> letterScores = new Dictionary<char, int>() { {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, 
+		{'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1},
+		{'D', 2}, {'G', 2},
+		{'B', 3}, {'C', 3}, {'M', 3}, {'P', 3},
+		{'F', 4}, {'H', 4}, {'V', 4}, {'W', 4}, {'Y', 4},
+		{'K', 5},
+		{'J', 8}, {'X', 8},
+		{'Q',10}, {'Z',10}, };
+
 			Console.WriteLine("Enter a word:");
 			string userInput = Console.ReadLine();
 			int count = 0;
 			int userInputLength = userInput.Length;
-			char[] letterArray = userInput.ToCharArray(0, userInputLength);
-			
-			foreach (char letter in letterArray)
-			{
-				if (letter == letterScores{0})
+			string word = userInput.ToUpper();
+
+
+			for (int i = 0; i < userInputLength; i ++)
 				{
-					count = count + letterScores{1};
+					foreach(KeyValuePair<char, int> kvp in letterScores)
+					{
+						Console.WriteLine(kvp.Key);
+						if (kvp.Key == (word[i]))
+						{
+							count += kvp.Value;
+							break;
+						}
+					}
 				}
-			}	
-			// //or
-			// for (int i = 0; i <= userInputLength; i ++)
+				Console.WriteLine(count);
+						// foreach (char letter in letterArray)
+			// {
+			// 	if (letter == letterScores{0})
 			// 	{
-			// 		if (letterScores.ContainsKey("a"))
-			// 		{
-			// 			count ++;
-			// 			//letterScores{0}
-			// 		}
-			// 		else{}
+			// 		count = count + letterScores{1};
 			// 	}
-			
-			Console.WriteLine(count);
+			// }	
+
+			//.Where((letter) => letters.ContainsKey(letter))
+			// //or	
 		}
 	}
 }
--- ScrabbleScore/Models/ScrabbleScore.cs
+++ ScrabbleScore/Models/ScrabbleScore.cs
@@ -9,4 +9,10 @@
 // 	{
 // 	// 	Dictionary<string, int> letterScores = new Dictionary<string, int>() { {"a",1 }, {"e", 1},{"i",1} };
 // 	}
+// 				public static int Score(string userInput)
+// 			{
+// 				return userInput
+// 				.Where((letter) => letters.ContainsKey(letter))
+// 				.Aggregate (0, (total, letter) => total + letters[letter]);
+// 			}
 // }
